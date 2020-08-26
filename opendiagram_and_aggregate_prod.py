1b2955eace06c0f86e4c52b4f247821ab9499cb8
raveman179 <telebit179@gmail.com>
2020-08-27 01:00:59 +0900

    add get_prodname, get_diagram_num

--- opendiagram_and_aggregate_prod.py
+++ opendiagram_and_aggregate_prod.py
@@ -16,25 +16,61 @@ import PySimpleGUI as sg
 import openpyxl
 from os.path import basename
 import subprocess
+import re
 
+def get_prodname(order_num):
+    """
+    受注No.検索処理
+
+    xlsx形式の生産計画シートを開いて品名を返す
+    
+    args: Fs受注No.(品質経歴書バーコード⑨)
+    return: 該当受注No.の品目名
+    """
+    wb = openpyxl.load_workbook('./production_schedule.xlsx')
+    sheet = wb.active 
+
+    prod_sheet = wb['Sheet1']
+    prod_name = ""
+
+    for row in prod_sheet.iter_rows(min_row=2):
+        if order_num == row[0].value:
+            row1 = row[1].value
+            prod_name = re.sub(r'\s.*ΩJ', '', row1)
+            sheet[row[7].coordinate] = "作業済み"
+            break
+
+    wb.save('production_schedule.xlsx')
+
+    return prod_name
+
+def get_diagram_num(prod_name):
+    """
+    図面ファイル検索処理
+
+    変換テーブルから図面のファイル名を取得する
+
+    args: get_prodname()で取得した製品名
+    return: 開こうとするPDF図面の絶対パス
+    """
+    # ------図面フォルダのパス
+    diagram_dir_path = "図面の入っているフォルダ名"
+
+    # ------変換テーブルで取得した図面No.
+    diagram_filename = ""
+
+    wb = openpyxl.load_workbook('./diagramnum.xlsx')
+
+    sheet = wb['Sheet1']
+    for row in sheet.iter_rows(min_row=2):
+        if prod_name == row[0].value:
+            diagram_filename = row[1].value
+    
+    pdf_abs_path = diagram_dir_path + diagram_filename
+
+    return pdf_abs_path
 
 sg.theme('Dark Green 1')
-
-# ------生産計画シートのパス
-prod_schedule_path = ""
-
-# ------品名<-->図面No.変換テーブルのパス
-pd_conv_table_path = ""
-
-# ------図面フォルダのパス
-diagram_dir_path = "C:\Users\Desktop\[図面の入っているフォルダ名]"
-
-# ------変換テーブルで取得した図面No.
-diagram_num = ""
-
-# ------開こうとするPDF図面の絶対パス
-pdf_abs_path = diagram_dir_path + diagram_num + ".pdf"
-
 
 layout = [
     [sg.Text('     受注No.(品質経歴書⑨)バーコードを読み込む', pad=((10,10),(20,20))), sg.InputText(key='order_num')],
@@ -45,26 +81,21 @@ window = sg.Window('生産計画、図面チェックプログラム(仮)', layo
 
 while True:
     event, values = window.read()
-    # print('イベント:', event ,', 値:',values) # 確認表示
+    print('イベント:', event ,', 値:',values) # 確認表示
+    
+    prod_name = get_prodname(values['order_num'])
 
-# 3."生産計画シート"のFS受注No.の列から入力された受注Noを検索する。
-#     受注No.が存在するなら
-#         最後の列'済'欄にチェックして、
-#         品目名のセルに入っている値を取得。ー＞変数:prod_nameへ代入する。
-#     存在しないなら
-#         "受注No.が存在しません"とかエラーを返す。
-
-
-# 図面のフルパスを投げてpopen関数で図面ファイルを開く
-    if event == 'open_diagram':
-
-
+    # 生産計画シート内に読み込んだFS受注No.が存在しない
+    
+    if prod_name == '':    
+        sg.Popup('受注No.が存在しません')
+        #閉じるときに一回開く。意図しない動作なので修正。
         
+    pdf_abs_path = get_diagram_num(prod_name)
+    
+    # 図面のフルパスを投げてpopen関数で図面ファイルを開く
+    if event == 'open_diagram':
         subprocess.Popen(['start', pdf_abs_path], shell=True)
-
-
-
-
 
     if event == sg.WIN_CLOSED or event == 'Quit':
         break
--- read_xlsxsheet.py
+++ read_xlsxsheet.py
@@ -1,13 +1,32 @@
 import openpyxl
 from pprint import pprint
+import re
+# diagram_number = openpyxl.load_workbook('./diagramNo.xlsx')
 
-diagram_number = openpyxl.load_workbook('./diagramNo.xlsx')
+# sheet = diagram_number['Sheet1']
+# cell = sheet['A1:A10']
 
-# cell = diagram_number.cell(column=2)
-sheet = diagram_number['Sheet1']
-cell = sheet['A1:A10']
+# pprint(cell.value)
+def open_prod_schedule(order_num):
+    wb = openpyxl.load_workbook('./production_schedule.xlsx')
+    sheet = wb.active 
 
-pprint(cell.value)
+    prod_sheet = wb['Sheet1']
+    prod_name = ""
+    fixed = ""
 
+    for row in prod_sheet.iter_rows(min_row=2):
+        if row[0].value == order_num:
+            row1 = row[1].value
+            prod_name = re.sub(r'\s.*ΩJ', '', row1)
+            sheet[row[7].coordinate] = "fixed"
+            break
+    else:
+        print('該当する受注No.がありません')
 
-# print(diagram_number['Sheet1']['B2'].value)
+    wb.save('production_schedule.xlsx')
+
+    return prod_name
+
+print(open_prod_schedule('07463620'))
+
